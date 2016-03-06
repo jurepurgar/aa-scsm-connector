c8c06b8f12fab2e8333d19bb8dffa09dc5935417
Jure Purgar <jure@purgar.net>
2016-03-06 20:18:12 +0100

    init

---
+++ PurgarNET.AAConnector/PurgarNET.AAConnector.Client/AuthorizationManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace PurgarNET.AAConnector.Client
+{
+    public class AuthorizationManager
+    {
+        private static object _lck = new object();
+        private static object _clientLck = new object();
+
+        private static WebClient _tokenClient = null;
+        private static WebClient tokenClient {
+            get
+            {
+                lock (_clientLck)
+                {
+                    if (_tokenClient == null)
+                    {
+                        _tokenClient = new WebClient();
+                        _tokenClient.Headers.Add("Accept", "application/json;odata=verbose;charset=utf-8");
+                    }
+                    return _tokenClient;
+                }
+            }
+        }       
+
+        
+        
+
+
+
+
+    }
+
+
+
+}
---
+++ PurgarNET.AAConnector/PurgarNET.AAConnector.Client/RestExtensions.cs
@@ -0,0 +1,44 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PurgarNET.AAConnector.Client
+{
+    public static class RestExtensions
+    {
+
+        public static Task<IRestResponse> GetResponseAsync(this RestClient client, IRestRequest request)
+        {
+            var tsc = new TaskCompletionSource<IRestResponse>();
+
+            var handle = client.ExecuteAsync(request, (response) =>
+            {
+                tsc.SetResult(response);
+            });
+
+            return tsc.Task;
+        }
+
+        public static Task<IRestResponse<T>> GetResponseAsync<T>(this RestClient client, IRestRequest request) where T : new()
+        {
+            var tsc = new TaskCompletionSource<IRestResponse<T>>();
+
+            var handle = client.ExecuteAsync<T>(request, (response) =>
+            {
+                if (response.ErrorException != null)
+                    throw response.ErrorException;
+
+                tsc.SetResult(response);
+            });
+
+            return tsc.Task;
+        }
+
+
+
+
+    }
+}
